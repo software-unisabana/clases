2b4174c290576f0a43973fe3568db607093d0818
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-01-25 19:38:21 -0500

    Initial commit

---
+++ src/main/java/co/edu/unisabana/clases/Clases20241Application.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.clases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clases20241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clases20241Application.class, args);
+	}
+
+}
